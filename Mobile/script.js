0107900c0ea85b5cf561504f72779293da5108d1
4spurkee <asparkasparki@gmail.com>
2025-03-01 17:31:10 +0200

    Add files via upload

---
+++ Mobile/script.js
@@ -0,0 +1,13 @@
+const disp = document.getElementById('display');
+function toDisp(button){
+    if(button.innerText == '-'){
+        if(disp.value == '') return;
+        if(disp.value[disp.value.length-1] == '+' ) return ;
+      
+        
+    }
+    disp.value += button.innerText;
+}
+function clr(){disp.value = '';}
+function calc(){disp.value = eval(disp.value);}
+function back() {disp.value = disp.value.slice(0, -1);}
